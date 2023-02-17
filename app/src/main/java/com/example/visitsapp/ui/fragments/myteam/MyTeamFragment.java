ac611f96d07dda976100fb2015b517a9c5b54e51
umer_jamil <umeru920@gmail.com>
2023-02-17 10:45:52 +0500

    changes 17/02/1022

--- app/src/main/java/com/example/visitsapp/ui/fragments/myteam/MyTeamFragment.java
+++ app/src/main/java/com/example/visitsapp/ui/fragments/myteam/MyTeamFragment.java
@@ -2,22 +2,20 @@ package com.example.visitsapp.ui.fragments.myteam;
 
 import android.os.Build;
 import android.os.Bundle;
-
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AlertDialog;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.visitsapp.R;
 import com.example.visitsapp.business.impl.Business;
 import com.example.visitsapp.delegate.ResponseCallBack;
-import com.example.visitsapp.model.responce.GetReportingTeamResponce;
 import com.example.visitsapp.model.responce.ReportingTeam;
 import com.example.visitsapp.ui.MainActivity;
 import com.example.visitsapp.ui.fragments.BaseFragment;
@@ -26,8 +24,8 @@ import com.example.visitsapp.utils.alert.AlertUtils;
 
 import java.text.Format;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 
 public class MyTeamFragment extends BaseFragment {
@@ -41,10 +39,12 @@ public class MyTeamFragment extends BaseFragment {
 
     private TextView tvDate1, tvDate2, tvDate3, tvDate4, tvDate5, tvDate6, tvDate7;
     private TextView tvday1, tvday2, tvday3, tvday4, tvday5, tvday6, tvday7;
+    String[] days = new String[] { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 
 
-    private LocalDate currentdate;
-
+    String currentDate ="";
+    Calendar cal1 = Calendar.getInstance();
     public MyTeamFragment(MainActivity mainActivity) {
         this.context = mainActivity;
     }
@@ -59,45 +59,120 @@ public class MyTeamFragment extends BaseFragment {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_my_team, container, false);
 
-        currentdate = LocalDate.now();
+        Calendar cal = Calendar.getInstance();
+
+        currentDate = format.format(cal.get(Calendar.DATE));
+
+
         recyclerView = view.findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(context, RecyclerView.VERTICAL, false));
 
         tvDate1 = view.findViewById(R.id.date1);
-        tvDate1.setText(String.valueOf(currentdate.getDayOfMonth()));
+        tvDate1.setText(String.valueOf(cal.get(Calendar.DAY_OF_MONTH)));
         tvDate2 = view.findViewById(R.id.date2);
-        tvDate2.setText(String.valueOf(currentdate.plusDays(1).getDayOfMonth()));
+        cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, 1);
+        tvDate2.setText(String.valueOf(cal.get(Calendar.DAY_OF_MONTH)));
+
         tvDate3 = view.findViewById(R.id.date3);
-        tvDate3.setText(String.valueOf(currentdate.plusDays(2).getDayOfMonth()));
+        cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, 2);
+        tvDate3.setText(String.valueOf(cal.get(Calendar.DAY_OF_MONTH)));
+
         tvDate4 = view.findViewById(R.id.date4);
-        tvDate4.setText(String.valueOf(currentdate.plusDays(3).getDayOfMonth()));
+        cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, 3);
+        tvDate4.setText(String.valueOf(cal.get(Calendar.DAY_OF_MONTH)));
+
         tvDate5 = view.findViewById(R.id.date5);
-        tvDate5.setText(String.valueOf(currentdate.plusDays(4).getDayOfMonth()));
+        cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, 4);
+        tvDate5.setText(String.valueOf(cal.get(Calendar.DAY_OF_MONTH)));
+
         tvDate6 = view.findViewById(R.id.date6);
-        tvDate6.setText(String.valueOf(currentdate.plusDays(5).getDayOfMonth()));
+        cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, 5);
+        tvDate6.setText(String.valueOf(cal.get(Calendar.DAY_OF_MONTH)));
+
         tvDate7 = view.findViewById(R.id.date7);
-        tvDate7.setText(String.valueOf(currentdate.plusDays(6).getDayOfMonth()));
+        cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, 6);
+        tvDate7.setText(String.valueOf(cal.get(Calendar.DAY_OF_MONTH)));
 
         tvday1 = view.findViewById(R.id.day1);
-        tvday1.setText(String.valueOf(currentdate.getDayOfWeek()).substring(0,3));
+        cal = Calendar.getInstance();
+
+        tvday1.setText(String.valueOf(days[cal.get(Calendar.DAY_OF_WEEK)-1]));
+
         tvday2 = view.findViewById(R.id.day2);
-        tvday2.setText(String.valueOf(currentdate.plusDays(1).getDayOfWeek()).substring(0,3));
+        cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, 1);
+        tvday2.setText(String.valueOf(days[cal.get(Calendar.DAY_OF_WEEK)-1]));
+
         tvday3 = view.findViewById(R.id.day3);
-        tvday3.setText(String.valueOf(currentdate.plusDays(2).getDayOfWeek()).substring(0,3));
+        cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, 2);
+        tvday3.setText(String.valueOf(days[cal.get(Calendar.DAY_OF_WEEK)-1]));
+
         tvday4 = view.findViewById(R.id.day4);
-        tvday4.setText(String.valueOf(currentdate.plusDays(3).getDayOfWeek()).substring(0,3));
+        cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, 3);
+        tvday4.setText(String.valueOf(days[cal.get(Calendar.DAY_OF_WEEK)-1]));
+
         tvday5 = view.findViewById(R.id.day5);
-        tvday5.setText(String.valueOf(currentdate.plusDays(4).getDayOfWeek()).substring(0,3));
+        cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, 4);
+        tvday5.setText(String.valueOf(days[cal.get(Calendar.DAY_OF_WEEK)-1]));
+
         tvday6 = view.findViewById(R.id.day6);
-        tvday6.setText(String.valueOf(currentdate.plusDays(5).getDayOfWeek()).substring(0,3));
+        cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, 5);
+        tvday6.setText(String.valueOf(days[cal.get(Calendar.DAY_OF_WEEK)-1]));
+
         tvday7 = view.findViewById(R.id.day7);
-        tvday7.setText(String.valueOf(currentdate.plusDays(6).getDayOfWeek()).substring(0,3));
+        cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, 6);
+        tvday7.setText(String.valueOf(days[cal.get(Calendar.DAY_OF_WEEK)-1]));
 
 
+//
+//
+//        tvDate1 = view.findViewById(R.id.date1);
+//        tvDate1.setText(String.valueOf(currentdate.getDayOfMonth()));
+//        tvDate2 = view.findViewById(R.id.date2);
+//        tvDate2.setText(String.valueOf(currentdate.plusDays(1).getDayOfMonth()));
+//        tvDate3 = view.findViewById(R.id.date3);
+//        tvDate3.setText(String.valueOf(currentdate.plusDays(2).getDayOfMonth()));
+//        tvDate4 = view.findViewById(R.id.date4);
+//        tvDate4.setText(String.valueOf(currentdate.plusDays(3).getDayOfMonth()));
+//        tvDate5 = view.findViewById(R.id.date5);
+//        tvDate5.setText(String.valueOf(currentdate.plusDays(4).getDayOfMonth()));
+//        tvDate6 = view.findViewById(R.id.date6);
+//        tvDate6.setText(String.valueOf(currentdate.plusDays(5).getDayOfMonth()));
+//        tvDate7 = view.findViewById(R.id.date7);
+//        tvDate7.setText(String.valueOf(currentdate.plusDays(6).getDayOfMonth()));
+//
+//        tvday1 = view.findViewById(R.id.day1);
+//        tvday1.setText(String.valueOf(currentdate.getDayOfWeek()).substring(0,3));
+//        tvday2 = view.findViewById(R.id.day2);
+//        tvday2.setText(String.valueOf(currentdate.plusDays(1).getDayOfWeek()).substring(0,3));
+//        tvday3 = view.findViewById(R.id.day3);
+//        tvday3.setText(String.valueOf(currentdate.plusDays(2).getDayOfWeek()).substring(0,3));
+//        tvday4 = view.findViewById(R.id.day4);
+//        tvday4.setText(String.valueOf(currentdate.plusDays(3).getDayOfWeek()).substring(0,3));
+//        tvday5 = view.findViewById(R.id.day5);
+//        tvday5.setText(String.valueOf(currentdate.plusDays(4).getDayOfWeek()).substring(0,3));
+//        tvday6 = view.findViewById(R.id.day6);
+//        tvday6.setText(String.valueOf(currentdate.plusDays(5).getDayOfWeek()).substring(0,3));
+//        tvday7 = view.findViewById(R.id.day7);
+//        tvday7.setText(String.valueOf(currentdate.plusDays(6).getDayOfWeek()).substring(0,3));
+//
+//
 
 
         setWeekView(view);
-        getReportingTeam(String.valueOf(currentdate));
+        cal1 = Calendar.getInstance();
+        getReportingTeam(String.valueOf(cal1.getTime()));
 
         return view;
     }
@@ -150,6 +225,8 @@ public class MyTeamFragment extends BaseFragment {
     @RequiresApi(api = Build.VERSION_CODES.O)
     private void setWeekView(View view) {
 
+
+
         v1 = view.findViewById(R.id.v1);
         v2 = view.findViewById(R.id.v2);
         v3 = view.findViewById(R.id.v3);
@@ -179,7 +256,10 @@ public class MyTeamFragment extends BaseFragment {
                 ll6.setBackground(context.getDrawable(R.drawable.team_date_unselect_bg));
                 ll7.setBackground(context.getDrawable(R.drawable.team_date_unselect_bg));
 
-                getReportingTeam(String.valueOf(currentdate));
+                cal1 = Calendar.getInstance();
+
+
+                getReportingTeam(String.valueOf(format.format(cal1.getTime())));
 
             }
         });
@@ -205,7 +285,10 @@ public class MyTeamFragment extends BaseFragment {
                 ll6.setBackground(context.getDrawable(R.drawable.team_date_unselect_bg));
                 ll7.setBackground(context.getDrawable(R.drawable.team_date_unselect_bg));
 
-                getReportingTeam(String.valueOf(currentdate.plusDays(1)));
+                cal1 = Calendar.getInstance();
+                cal1.add(Calendar.DATE, 1);
+
+                getReportingTeam(String.valueOf(format.format(cal1.getTime())));
             }
         });
 
@@ -230,7 +313,10 @@ public class MyTeamFragment extends BaseFragment {
                 ll6.setBackground(context.getDrawable(R.drawable.team_date_unselect_bg));
                 ll7.setBackground(context.getDrawable(R.drawable.team_date_unselect_bg));
 
-                getReportingTeam(String.valueOf(currentdate.plusDays(2)));
+                cal1 = Calendar.getInstance();
+                cal1.add(Calendar.DATE, 2);
+
+                getReportingTeam(String.valueOf(format.format(cal1.getTime())));
             }
         });
 
@@ -253,7 +339,11 @@ public class MyTeamFragment extends BaseFragment {
                 ll5.setBackground(context.getDrawable(R.drawable.team_date_unselect_bg));
                 ll6.setBackground(context.getDrawable(R.drawable.team_date_unselect_bg));
                 ll7.setBackground(context.getDrawable(R.drawable.team_date_unselect_bg));
-                getReportingTeam(String.valueOf(currentdate.plusDays(3)));
+
+                cal1 = Calendar.getInstance();
+                cal1.add(Calendar.DATE, 3);
+
+                getReportingTeam(String.valueOf(format.format(cal1.getTime())));
             }
         });
 
@@ -278,7 +368,10 @@ public class MyTeamFragment extends BaseFragment {
                 ll6.setBackground(context.getDrawable(R.drawable.team_date_unselect_bg));
                 ll7.setBackground(context.getDrawable(R.drawable.team_date_unselect_bg));
 
-                getReportingTeam(String.valueOf(currentdate.plusDays(4)));
+                cal1 = Calendar.getInstance();
+                cal1.add(Calendar.DATE, 4);
+
+                getReportingTeam(String.valueOf(format.format(cal1.getTime())));
             }
         });
 
@@ -302,7 +395,10 @@ public class MyTeamFragment extends BaseFragment {
                 ll6.setBackground(context.getDrawable(R.drawable.team_date_select_bg));
                 ll7.setBackground(context.getDrawable(R.drawable.team_date_unselect_bg));
 
-                getReportingTeam(String.valueOf(currentdate.plusDays(5)));
+                cal1 = Calendar.getInstance();
+                cal1.add(Calendar.DATE, 5);
+
+                getReportingTeam(String.valueOf(format.format(cal1.getTime())));
             }
         });
 
@@ -326,7 +422,10 @@ public class MyTeamFragment extends BaseFragment {
                 ll6.setBackground(context.getDrawable(R.drawable.team_date_unselect_bg));
                 ll7.setBackground(context.getDrawable(R.drawable.team_date_select_bg));
 
-                getReportingTeam(String.valueOf(currentdate.plusDays(6)));
+                cal1 = Calendar.getInstance();
+                cal1.add(Calendar.DATE, 6);
+
+                getReportingTeam(String.valueOf(format.format(cal1.getTime())));
             }
         });
     }
